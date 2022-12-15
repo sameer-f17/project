ae330fbbd6c3b96b0eff4bcf2062435da622ca72
sameer-f17 <120626273+sameer-f17@users.noreply.github.com>
2022-12-15 12:18:10 +0500

    Add files via upload

---
+++ last/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      
+    },
+  });
